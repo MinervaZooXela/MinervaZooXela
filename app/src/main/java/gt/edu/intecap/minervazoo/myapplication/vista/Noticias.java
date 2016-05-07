21aa7e1a18e1fd3113fbd9abce1f073c699c1e81
EduardoColon <eduardo.colon.15@gmail.com>
2016-05-07 13:43:20 -0600

    Commit Eduardo

--- app/src/main/java/gt/edu/intecap/minervazoo/myapplication/vista/Noticias.java
+++ app/src/main/java/gt/edu/intecap/minervazoo/myapplication/vista/Noticias.java
@@ -2,9 +2,12 @@ package gt.edu.intecap.minervazoo.myapplication.vista;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.ViewFlipper;
 
 import gt.edu.intecap.minervazoo.myapplication.R;
@@ -13,6 +16,7 @@ public class Noticias extends Fragment {
 
     private static final int INTERVALO = 5000;
     private ViewFlipper flipper;
+    private TextView paginaOficial;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,7 +34,10 @@ public class Noticias extends Fragment {
         flipper.setFlipInterval(INTERVALO);
         flipper.startFlipping();
 
+        paginaOficial = (TextView) vista.findViewById(R.id.tvPaginaOficial);
+        paginaOficial.setMovementMethod(LinkMovementMethod.getInstance());
+        String text = "<a href='http://www.google.com'> Google </a>";
+        paginaOficial.setText(Html.fromHtml(text));
         return vista;
     }
-
 }
