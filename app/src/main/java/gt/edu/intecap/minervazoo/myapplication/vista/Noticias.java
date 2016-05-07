5eedc3eecbab5893da4cf7645408651551d8803f
EduardoColon <eduardo.colon.15@gmail.com>
2016-05-07 11:59:52 -0600

    Commit Eduardo

--- app/src/main/java/gt/edu/intecap/minervazoo/myapplication/vista/Noticias.java
+++ app/src/main/java/gt/edu/intecap/minervazoo/myapplication/vista/Noticias.java
@@ -5,15 +5,32 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ViewFlipper;
 
 import gt.edu.intecap.minervazoo.myapplication.R;
 
 public class Noticias extends Fragment {
 
+    private static final int INTERVALO = 5000;
+    private ViewFlipper flipper;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View vista = inflater.inflate(R.layout.fragment_noticias, container, false);
+
+        flipper = (ViewFlipper)vista.findViewById(R.id.flipper);
+        flipper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flipper.showNext();
+            }
+        });
+
+        flipper.setFlipInterval(INTERVALO);
+        flipper.startFlipping();
+
         return vista;
     }
+
 }
