3c628d1bf1741d8f624939cc201f2b386a0b74aa
LuisRL <valle2137@gmail.com>
2016-05-09 14:46:47 -0600

    commitR

--- app/src/main/java/gt/edu/intecap/minervazoo/myapplication/controlador/MainActivity.java
+++ app/src/main/java/gt/edu/intecap/minervazoo/myapplication/controlador/MainActivity.java
@@ -26,7 +26,7 @@ import gt.edu.intecap.minervazoo.myapplication.vista.Noticias;
 import gt.edu.intecap.minervazoo.myapplication.vista.Proyecto;
 import gt.edu.intecap.minervazoo.myapplication.vista.Tour;
 import gt.edu.intecap.minervazoo.myapplication.vista.Ubicacion;
-import gt.edu.intecap.minervazoo.myapplication.vista.YouTube;
+import gt.edu.intecap.minervazoo.myapplication.vista.actividad.YouTube;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -37,15 +37,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -81,12 +72,8 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
