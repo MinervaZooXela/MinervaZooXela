5315e28cb60bcc7983fc95534c2032eec042e821
mauricio360 <mauricioayau@gmail.com>
2016-05-09 19:10:39 -0600

    Commit

--- app/src/main/java/gt/edu/intecap/minervazoo/myapplication/vista/Tour.java
+++ app/src/main/java/gt/edu/intecap/minervazoo/myapplication/vista/Tour.java
@@ -13,39 +13,13 @@ import gt.edu.intecap.minervazoo.myapplication.R;
 public class Tour extends Fragment {
 
     ImageView imageView;
-    ZoomControls zoomControls;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View vista = inflater.inflate(R.layout.fragment_tour, container, false);
 
-        imageView = (ImageView) vista.findViewById(R.id.imageView);
-        zoomControls = (ZoomControls) vista.findViewById(R.id.zoomControls);
 
-        zoomControls.setOnZoomInClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                float x = imageView.getScaleX();
-                float y = imageView.getScaleY();
-
-                imageView.setScaleX(x + 1);
-                imageView.setScaleY(y + 1);
-            }
-        });
-
-        zoomControls.setOnZoomOutClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                float x = imageView.getScaleX();
-                float y = imageView.getScaleY();
-
-                imageView.setScaleX(x - 1);
-                imageView.setScaleY(y - 1);
-            }
-        });
 
 
         return vista;
